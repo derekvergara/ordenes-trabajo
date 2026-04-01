dd55478cdf030005477a874ceee775dd46a5e239
Derek Vergara <dereckvergara12@gmail.com>
2026-03-31 19:12:36 -0500

    creacion del backend en spring boot
    Developer Derek Vergara

---
+++ ordenes_trabajo_backend/src/main/java/ord/ordenes_trabajo_backend/OrdenesTrabajoBackendApplication.java
@@ -0,0 +1,13 @@
+package ord.ordenes_trabajo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenesTrabajoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenesTrabajoBackendApplication.class, args);
+	}
+
+}
---
+++ ordenes_trabajo_backend/src/test/java/ord/ordenes_trabajo_backend/OrdenesTrabajoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ord.ordenes_trabajo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenesTrabajoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
